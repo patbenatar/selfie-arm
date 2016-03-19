8d8cec9a09442273e039b06e8fc9b98839ebd903
Nick Giancola <nick@gophilosophie.com>
2016-03-19 07:52:48 -0700

    Initial Commit

---
+++ Selfie ArmUITests/Selfie_ArmUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Selfie_ArmUITests.swift
+//  Selfie ArmUITests
+//
+//  Created by Nick Giancola on 3/19/16.
+//  Copyright © 2016 Nick Giancola. All rights reserved.
+//
+
+import XCTest
+
+class Selfie_ArmUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
